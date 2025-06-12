b7f70faaef186779a99ecdc87fb749a48aef76ad
Redmoor <danilamandrabura@gmail.com>
2025-06-13 01:15:15 +0300

    feat: Build graph of messages and tags

---
+++ features/graph/builder.go
@@ -0,0 +1,77 @@
+package graph
+
+import (
+	"fmt"
+	"shellshift/internal/db"
+	"slices"
+)
+
+type NodeData struct {
+	ID string `json:"id"`
+}
+
+type EdgeData struct {
+	ID     string `json:"id"`
+	Source string `json:"source"`
+	Target string `json:"target"`
+  Tag string `json:"tag"`
+}
+
+type Node struct {
+	Group string `json:"group"`
+	Data  NodeData `json:"data"`
+}
+
+type Edge struct {
+	Group string `json:"group"`
+	Data  EdgeData `json:"data"`
+}
+
+func buildGraph(chats []db.GetGraphRow) []any {
+	m := make(map[string][]string)
+
+	var graph []any
+
+	for _, v := range chats {
+		tag := v.Name
+		id := v.ID
+
+		// Build [tag]:[]ids map
+		if s, ok := m[tag]; !ok {
+			m[tag] = []string{id}
+		} else {
+			m[tag] = append(s, id)
+		}
+
+		// Push nodes to graph slice
+		if !slices.ContainsFunc(graph, func(node any) bool {
+			return node.(Node).Data.ID == v.ID
+		}) {
+			graph = append(graph, Node{
+				Group: "nodes",
+				Data: NodeData{
+					ID: v.ID,
+				},
+			})
+		}
+	}
+
+	// Collect edges
+	for tag, ids := range m {
+		for i := range ids {
+			for j := i + 1; j < len(ids); j++ {
+				graph = append(graph, Edge{
+					Group: "edges",
+					Data: EdgeData{
+						ID:     fmt.Sprintf("%s:%s", ids[i], ids[j]),
+						Source: ids[i],
+						Target: ids[j],
+            Tag: tag,
+					},
+				})
+			}
+		}
+	}
+
+	return graph
+}
