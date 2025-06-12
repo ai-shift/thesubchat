f3a8fe7be1c601c7101214e5c4ac00c5ccbfae05
Redmoor <danilamandrabura@gmail.com>
2025-06-12 13:38:45 +0300

    feat: Add timestamps to the chat table

--- sql/migrations/1749514386_init.up.sql
+++ sql/migrations/1749514386_init.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS chat (
     id TEXT PRIMARY KEY,
     title TEXT NOT NULL,
-    messages BLOB NOT NULL
+    messages BLOB NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (unixepoch()),
+    updated_at INTEGER NOT NULL DEFAULT (unixepoch())
 );
--- sql/migrations/schema.sql
+++ sql/migrations/schema.sql
@@ -8,5 +8,7 @@ CREATE TABLE sqlite_sequence(name, seq);
 CREATE TABLE chat (
     id TEXT PRIMARY KEY,
     title TEXT NOT NULL,
-    messages BLOB NOT NULL
+    messages BLOB NOT NULL,
+    created_at INTEGER NOT NULL DEFAULT (unixepoch()),
+    updated_at INTEGER NOT NULL DEFAULT (unixepoch())
 );
