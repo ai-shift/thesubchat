28ab3fd839a23310f441173d79cffae91f447975
Redmoor <danilamandrabura@gmail.com>
2025-06-17 00:12:08 +0300

    feat: Add mention table and cascade deletes

--- sql/migrations/1749514386_init.down.sql
+++ sql/migrations/1749514386_init.down.sql
@@ -1,3 +1,5 @@
-DROP TABLE chat;
-
 DROP TABLE chat_tag;
+
+DROP TABLE mention;
+
+DROP TABLE chat;
--- sql/migrations/1749514386_init.up.sql
+++ sql/migrations/1749514386_init.up.sql
@@ -6,9 +6,17 @@ CREATE TABLE IF NOT EXISTS chat (
     updated_at INTEGER NOT NULL DEFAULT (unixepoch())
 );
 
-CREATE TABLE chat_tag (
+CREATE TABLE IF NOT EXISTS chat_tag (
     chat_id text NOT NULL,
     name text NOT NULL,
     PRIMARY KEY (chat_id, name),
-    FOREIGN KEY (chat_id) REFERENCES chat (id)
+    FOREIGN KEY (chat_id) REFERENCES chat (id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS mention (
+      source_id TEXT NOT NULL,
+      target_id TEXT NOT NULL CHECK (target_id != source_id),
+      PRIMARY KEY (source_id, target_id),
+      FOREIGN KEY (source_id) REFERENCES chat (id) ON DELETE CASCADE,
+      FOREIGN KEY (target_id) REFERENCES chat (id) ON DELETE CASCADE
+)
