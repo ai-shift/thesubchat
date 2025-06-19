13b33260515fefc514f578978f013d4eae11d367
scipunch <scipunch@gmail.com>
2025-06-19 11:32:23 +0200

    chore: Get port from env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -41,8 +41,12 @@ func main() {
 	m.Handle(fmt.Sprintf("%s/", graphURI), http.StripPrefix(graphURI, graph.InitMux(q, protector, chatURI)))
 	m.Handle(fmt.Sprintf("%s/", authURI), http.StripPrefix(authURI, auth.InitMux(q, protector, loginURI, registerURI, graphURI)))
 
-	slog.Info("site running on port 3000...")
-	if err := http.ListenAndServe(":3000", m); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "42069"
+	}
+	slog.Info("site running", "port", port)
+	if err := http.ListenAndServe(":"+port, m); err != nil {
 		slog.Error("serving finished with", "err", err.Error())
 	}
 }
